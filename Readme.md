998222c3ad855c489c24c9b6245cfd4e9f152db3
fayolle <pafayolle@gmail.com>
2022-06-12 11:48:05 +0900

    Update the test file and add a readme file

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Reverse image filtering with clean and noisy filters 
+A simple implementation in MATLAB of this [paper]() on reverse image filtering noisy filters. 
+
+## Notes 
+The file 'test.m' contains a simple test for defiltering a noisy motion blurred image. The file 'Landweber.m' contains an implementation of the main approach described in the paper. The files 'PC_Landweber.m' and 'NA_Landweber.m' contain accelerated variants (described in the paper as well). 
+
+## Reference 
+Link to the [preprint]() where the methods were introduced and the corresponding bibtex entry 
+```
+@article{Wang2022,
+title = "Reverse Image Filtering with Clean and Noisy Filters",
+author = "Lizhong Wang and Pierre-Alain Fayolle and Alexander Belyaev",
+year = "2022",
+journal = "Signal, Image and Video Processing",
+issn = "1863-1703",
+publisher = "Springer",
+}
+```
--- test.m
+++ test.m
@@ -1,29 +1,35 @@
 x = im2double(imread('141012.jpg'));
 % x=im2double(imread('78098.jpg'));
 
+
 %  filter
-%H = im2double(rgb2gray(imread('testkernel2.bmp'))); H = H./sum(H(:));
 % H = fspecial('gaussian', [5 5],2);
 H = fspecial('motion',21,11);
 % H = fspecial('disk',5);
 % f=@(x) imfilter(x,H,'circular'); 
 f = @(x) imnoise(imfilter(x,H,'circular'),'gaussian',0,0.0001); 
 
-options.tau = 0.1;
-options.mu = 5e-5;
-options.maxiter = 2000;
-
 y = f(x);
 
+
+% General options for all the methods 
+options.tau = 0.1;
+options.mu = 5e-5;
+
+
+disp('Running Landweber iterations');
+options.maxiter = 2000;
 L = Landweber(f, y, options);
-%L2 = Landweber2(f, y, options);
 
-figure, imshow([x, y, L]);
-%figure, imshow([x, y, L2]);
+disp('Running Nesterov accelerated Landweber iterations');
+options.maxiter = 500;
+NAL = Nesterov_accelerated_Landweber(f, y, options); 
 
-%options.maxiter = 500;
-%NAL = Nesterov_accelerated_Landweber(f, y, options); 
-%options.maxiter = 1000;
-%PCL = PC_Landweber(f, y, options); 
-%figure, imshow([x, y, NAL]);
-%figure, imshow([x, y, PCL]);
+disp('Running phase corrected Landweber iterations');
+options.maxiter = 1000;
+PCL = PC_Landweber(f, y, options); 
+
+
+figure, imshow([x, y, L]), title('Landweber iterations');
+figure, imshow([x, y, NAL]), title('Nesterov accelerated Landweber iterations');
+figure, imshow([x, y, PCL]), title('Phase corrected Landweber iterations');
